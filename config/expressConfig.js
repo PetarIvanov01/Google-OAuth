736a83513297b2472cb478bd3825837639c0ecdf
Petar Ivanov <111183840+PetarIvanov01@users.noreply.github.com>
2023-07-04 18:33:31 +0300

    refactor: Update Express server configuration with Passport authentication and session

--- config/expressConfig.js
+++ config/expressConfig.js
@@ -2,17 +2,34 @@ const morgan = require('morgan');
 const exphbs = require('express-handlebars');
 const express = require('express');
 const path = require('path');
+const passport = require('passport');
+const session = require('express-session');
 
 module.exports = (app) => {
+    //Passport config
+    require('../config/passport')(passport);
 
+    //Showing stuff on the console when request is made
     if (process.env.NODE_ENV === 'development') {
-        //Showing stuff on the console when request is made
-        app.use(morgan('dev'))
+        app.use(morgan('dev'));
     }
 
+    //Handlebars Config
     app.engine('.hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }));
     app.set('view engine', '.hbs');
 
+    //Session Config
+    app.use(session({
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: false,
+    }))
+
+    //Passport middlewares
+    app.use(passport.initialize());
+    app.use(passport.session());
+
+    //Static files middleware
     app.use(express.static(path.join(__dirname, '../public')));
 
 }
