018274046526fbbca6ac2fac3de18620ee788fea
Petar Ivanov <111183840+PetarIvanov01@users.noreply.github.com>
2023-07-04 18:34:02 +0300

    Add Passport configuration for Google OAuth strategy
    This commit adds the Passport configuration for the Google OAuth strategy. The following changes were made:
    - Imported the necessary dependencies, including the GoogleStrategy from 'passport-google-oauth20'.
    - Configured the GoogleStrategy with the clientID, clientSecret, and callbackURL.
    - Defined the async callback function for the strategy, which saves the user profile using the saveUser function from the userService module.
    - Implemented the serializeUser and deserializeUser functions for session management.

---
+++ config/passport.js
@@ -0,0 +1,40 @@
+const { saveUser } = require('../services/userService');
+
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+module.exports = (passport) => {
+
+    passport.use(
+        new GoogleStrategy(
+            {
+                clientID: process.env.GOOGLE_CLIENT_ID,
+                clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+                callbackURL: '/auth/google/callback'
+            },
+            async (accessToken, refreshToken, profile, cb) => {
+                try {
+                    const user = await saveUser(profile)
+
+                    return cb(null, user);
+
+                } catch (error) {
+                    console.error(error);
+                }
+            }))
+
+    passport.serializeUser(function (user, cb) {
+        process.nextTick(function () {
+            return cb(null, {
+                id: user.id,
+                username: user.username,
+                picture: user.picture
+            });
+        });
+    });
+
+    passport.deserializeUser(function (user, cb) {
+        process.nextTick(function () {
+            return cb(null, user);
+        });
+    });
+}
