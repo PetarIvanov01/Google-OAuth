7aceb6f3502fcac53470ce582f82f8f2159777fc
Petar Ivanov <111183840+PetarIvanov01@users.noreply.github.com>
2023-07-05 12:34:30 +0300

    Update authentication routes with additional functionality and middlewares

--- controllers/loginAuth.js
+++ controllers/loginAuth.js
@@ -1,21 +1,30 @@
 const router = require('express').Router();
 const passport = require('passport');
+const { hasUser, isGuest } = require('../middlewares/guards');
+
 //Rendering login page
-
-router.get('/login', (req, res) => {
-
+router.get('/login', isGuest, (req, res) => {
     res.render('login', {
         layout: 'login'
     })
 
 })
+
 // Auth with Google
 router.get('/google', passport.authenticate('google', { scope: ['profile'] }));
 
+// Auth callback 
 router.get('/google/callback',
     passport.authenticate('google', { failureRedirect: '/auth/login' }),
     (req, res) => {
         res.redirect('/');
     })
 
+// Logout
+router.get('/logout', hasUser, (req, res) => {
+    req.logout((err) => {
+        if (err) { return next(err); }
+        res.redirect('/auth/login');
+    });
+})
 module.exports = router
