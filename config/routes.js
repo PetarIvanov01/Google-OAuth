3ef877a0fd34acadbc5e012eeb14dc92fca99f29
Petar Ivanov <111183840+PetarIvanov01@users.noreply.github.com>
2023-07-17 10:43:33 +0300

    Added stories route to application
    This commit message describes the change made in the code, which is the addition of the "stories" route to the application. By including this route, the application now has the capability to handle requests related to stories. and process story-related operations.

--- config/routes.js
+++ config/routes.js
@@ -1,9 +1,10 @@
 const dashboard = require('../controllers/dashboard');
 const authentication = require('../controllers/loginAuth');
-
+const stories = require('../controllers/stories')
 module.exports = (app) => {
 
     app.use('/', dashboard);
+    app.use('/stories', stories)
     app.use('/auth', authentication);
 
 }
