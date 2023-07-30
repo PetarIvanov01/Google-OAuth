20cf660846e80d590eb36580826ab57b1e8f49da
Petar Ivanov <111183840+PetarIvanov01@users.noreply.github.com>
2023-07-30 13:06:20 +0300

    Implement functions for fetching user stories and story details

--- services/storyService.js
+++ services/storyService.js
@@ -8,6 +8,29 @@ async function getAllStories(id) {
     }
 }
 
+async function getUserStories(userId) {
+    try {
+        return Story.find({ user: userId, status: 'public' }).populate('user').lean();
+    } catch (error) {
+        throw error
+    }
+}
+
+async function getFullStory(id) {
+    try {
+        return await Story.findById(id).populate('user').lean();
+    } catch (error) {
+        throw error
+    }
+}
+async function getStorieById(id) {
+    try {
+        return Story.findOne({ _id: id }).lean();
+    } catch (error) {
+        throw error
+    }
+}
+
 async function createStories(data) {
     try {
         const { title, status, body, user } = data;
@@ -26,7 +49,7 @@ async function createStories(data) {
 
 async function getPublicStories() {
     try {
-        return Story.find({ status:'public' })
+        return Story.find({ status: 'public' })
             .populate('user')
             .sort({ createdAt: 'desc' })
             .lean();
@@ -35,5 +58,22 @@ async function getPublicStories() {
         throw error
     }
 }
+async function updateStory(id, story) {
+    try {
+        return Story.findByIdAndUpdate({ _id: id }, story, {
+            new: true,
+            runValidators: true
+        })
+    } catch (error) {
+        throw error
+    }
+}
 
-module.exports = { getAllStories, createStories, getPublicStories }
+async function deleteStory(id) {
+    try {
+        await Story.findByIdAndDelete(id)
+    } catch (error) {
+        throw error
+    }
+}
+module.exports = { getAllStories, getUserStories,getFullStory, deleteStory, createStories, updateStory, getPublicStories, getStorieById }
