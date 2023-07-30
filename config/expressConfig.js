e15167effbf13cf4dae6df54a589896c4013c419
Petar Ivanov <111183840+PetarIvanov01@users.noreply.github.com>
2023-07-30 13:04:29 +0300

    Add method override middleware
    Implemented method override middleware to allow HTTP method tunneling in POST requests. The middleware looks for the '_method' parameter in urlencoded POST bodies and converts the request method accordingly, allowing clients to perform PUT and DELETE requests through a POST request with the '_method' parameter set to 'PUT' or 'DELETE'.

--- config/expressConfig.js
+++ config/expressConfig.js
@@ -4,7 +4,8 @@ const express = require('express');
 const path = require('path');
 const passport = require('passport');
 const session = require('express-session');
-const MongoStore = require('connect-mongo')
+const MongoStore = require('connect-mongo');
+const methodOverride = require('method-override');
 
 module.exports = (app) => {
     //Passport config
@@ -18,11 +19,21 @@ module.exports = (app) => {
     app.use(express.urlencoded({ extended: false }));
     app.use(express.json());
 
+    //Method override
+    app.use(methodOverride(function (req, res) {
+        if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+            // look in urlencoded POST bodies and delete it
+            let method = req.body._method
+            delete req.body._method
+            return method
+        }
+    }))
+
     //Handlebars Helpers
-    const { formatDate, truncate, stripTags, editIcon } = require('../helpers/hbs');
+    const { formatDate, truncate, stripTags, editIcon, select } = require('../helpers/hbs');
 
     //Handlebars Config
-    app.engine('.hbs', exphbs.engine({ helpers: { formatDate, editIcon, truncate, stripTags }, defaultLayout: 'main', extname: '.hbs' }));
+    app.engine('.hbs', exphbs.engine({ helpers: { formatDate, editIcon, truncate, stripTags, select }, defaultLayout: 'main', extname: '.hbs' }));
     app.set('view engine', '.hbs');
 
     //Session Config
