465a5b5d26ebf9b297eacfbec6cf5ffc57c0ecaa
Petar Ivanov <111183840+PetarIvanov01@users.noreply.github.com>
2023-07-30 13:05:29 +0300

    Add new story-related routes
    Introduced new routes for showing a full story, editing a story, updating a story, and deleting a story. The routes are protected by the `hasUser` middleware to ensure only authenticated users can access them. Additionally, implemented error handling to render appropriate error pages (404 or 500) in case of issues.

--- controllers/stories.js
+++ controllers/stories.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const { hasUser } = require('../middlewares/guards');
-const { createStories, getPublicStories } = require('../services/storyService');
+const Story = require('../models/Story');
+const { createStories, getPublicStories, getStorieById, updateStory, deleteStory, getFullStory, getUserStories } = require('../services/storyService');
 
 //Post
 //Creating a storie
@@ -39,11 +40,101 @@ router.get('/', hasUser, async (req, res) => {
         })
 
     } catch (error) {
-        res.render('error/500')
+        res.render('error/500');
+    }
+})
+
+//Show full story
+router.get('/:id', hasUser, async (req, res) => {
+    try {
+        const story = await getFullStory(req.params.id);
+
+        if (!story) {
+            return res.render('error/404');
+        }
+
+        res.render('stories/show', {
+            story
+        })
+    } catch (error) {
+        console.error(error)
+        res.render('error/404');
+    }
+})
+
+//Show edit storie
+router.get('/edit/:id', hasUser, async (req, res) => {
+
+    try {
+        const id = req.params.id;
+        const item = await getStorieById(id);
+
+        if (!item) {
+            return res.render('error/404')
+        }
+
+        if (item.user != req.user.id) {
+            res.redirect('/stories')
+        }
+        else {
+            res.render('stories/edit', {
+                item,
+            })
+        }
+
+    } catch (error) {
+        res.render('error/500');
+    }
+})
+
+//Update storie
+router.put('/:id', hasUser, async (req, res) => {
+
+    try {
+
+        let story = await getStorieById(req.params.id)
+
+        if (!story) {
+            return res.render('error/404')
+        }
+        if (story.user != req.user.id) {
+            res.redirect('/stories')
+        }
+        else {
+            story = await updateStory(req.params.id, req.body);
+
+        }
+        res.redirect('/dashboard');
+
+    } catch (error) {
+        console.error(error)
+        return res.render('error/500');
+    }
+})
+
+//DELETE storie
+router.delete('/:id', hasUser, async (req, res) => {
+    try {
+        await deleteStory(req.params.id);
+        res.redirect('/dashboard');
+    } catch (error) {
+        console.error(error)
+        return res.render('error/500');
     }
 })
 
 
+router.get('/user/:userId', hasUser, async (req, res) => {
+    try {
+        const stories = await getUserStories(req.params.userId);
 
+        res.render('stories/index', {
+            stories
+        })
+    } catch (error) {
+        console.error(error);
+        res.render('error/500')
+    }
+})
 
 module.exports = router
